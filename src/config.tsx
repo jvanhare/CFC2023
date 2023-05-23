51c349e38fb4c383b887ee7d43c7a533daf35e43
Julien Vanharen <julien.vanharen@inria.fr>
2023-05-23 14:38:43 +0200

    Initial commit

---
+++ src/config.tsx
@@ -0,0 +1,33 @@
+const config = {
+  title:
+    "7th AIAA CFD Drag Prediction Workshop results using anisotropic mesh adaptation",
+  shortTitle: "DPW7",
+  date: "2023-04-28",
+  place: "Cannes, France",
+  authors: [
+    {
+      name: "Julien Vanharen",
+      mail: "julien.vanharen@inria.fr",
+      website: "https://jvanhare.github.io",
+      affiliation: 1,
+    },
+    {
+      name: "Cosimo Tarsia Morisco",
+      mail: "cosimo.tarsia-morisco@inria.fr",
+      affiliation: 1,
+    },
+    {
+      name: "Frédéric Alauzet",
+      mail: "frederic.alauzet@inria.fr",
+      affiliation: 1,
+    },
+  ],
+  affiliation: [
+    "GAMMA Team - Inria Saclay Île-De-France - 91120 Palaiseau, France",
+  ],
+  speaker: "Julien Vanharen",
+  shortAuthor: "J. Vanharen",
+  conference: "IACM - CFC 2023",
+};
+
+export default config;
